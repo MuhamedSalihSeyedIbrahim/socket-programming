fa50fc7d6613bcc851aca84efa422e3dac2a1cbe
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2017-10-24 13:10:13 +0530

    Add files via upload

---
+++ echo_ server.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[])
+{
+  struct sockaddr_in server;
+  int serve=socket(PF_INET,SOCK_STREAM,0);
+    
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_port = htons(8080);
+
+  bind(serve, (struct sockaddr *)&server, sizeof(server));
+  listen(serve,3);
+    
+    
+  struct sockaddr_in client;
+  int clien = accept(serve, (struct sockaddr *)&client, (socklen_t *)&client);
+
+  char server_msg[1024], client_msg[1024];
+
+  recv(clien, client_msg, 1024, 0);
+  printf("%s",client_msg);
+
+  while (strcmp(server_msg,"~1")!=0)
+  {
+   
+    send(clien, server_msg, 1024, 0);
+    recv(clien, server_msg, 1024, 0);
+    
+  }
+}
